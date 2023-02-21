3299dc9676c9cf50cdf4be7fa44e1d85b584d50c
Chi-Ling-Lee <19940818jillian@gmail.com>
2023-02-21 21:33:34 +0800

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+  <div>
+    {{data}}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script>
+  export default {
+    data(){
+      return{
+        data:{}
+      }
+    },
+    mounted(){
+      const path = import.meta.env.VITE_PATH;
+      console.log(import.meta.env.VITE_TEXT);
+      this.$http.get(path).
+        then(({data})=>{
+          this.data = data.results[0];
+        }).catch((err) => {
+          //console.dir(err);
+        });
+    }
+  }
+</script>
